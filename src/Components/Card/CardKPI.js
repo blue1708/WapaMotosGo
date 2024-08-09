38973aeaed7333d18386863d989d64bb6eb5dbb4
LeoColors <vileo101@outlook.com>
2024-08-09 17:28:29 -0600

    actualizacion KPI card

--- src/Components/Card/CardKPI.js
+++ src/Components/Card/CardKPI.js
@@ -6,7 +6,7 @@ export const CardKPI = () => {
     <div>
             <Card className="mx-auto max-w-xs" decoration="top" decorationColor="indigo">
                 <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">kPI</p>
-                <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">$28,854</p>
+                <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">$30,854</p>
                 <ProgressBar value={72} />
             </Card>
     </div>
