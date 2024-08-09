3c7d44c75957ef6b782fdee884c568edbabef1de
Adrian <aitoivan04@gmail.com>
2024-08-08 18:46:18 -0600

    Actualizacion nombre WapaMotosGo en Topbar

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -29,7 +29,7 @@ const Topbar = () => {
                 <i className="pi pi-calendar" />
               </div>
               <LogoutButton/>
-              <p>Wapayasos</p>
+              <p>WapaMotosGo</p>
               {/* Agrega más información aquí */}
             </div>
           </div>
