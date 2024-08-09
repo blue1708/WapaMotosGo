0d05ee205b2f0ed4276750b916f96afe704f82a6
LeoColors <vileo101@outlook.com>
2024-08-09 17:36:56 -0600

    actualizacion Card Lol a Utilidades

--- src/Components/Card/CardLol.js
+++ src/Components/Card/CardLol.js
@@ -5,7 +5,7 @@ export const CardLol = () => {
     return (
     <div>
             <Card className="mx-auto max-w-xs" decoration="top" decorationColor="yellow">
-                <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Utilidad</p>
+                <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">Utilidades</p>
                 <p className="text-3xl text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">$15,743</p>
                 <ProgressBar color='yellow' value={72} />
             </Card>
